6f00eff7aa5258a208de8d9514e589c073ad2b6b
LeoAmaral <leoamaral1291@gmail.com>
2025-03-17 15:58:56 -0300

    Creating menu... Add music, background and text...

--- code/menu.py
+++ code/menu.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
 
-from Game import Game
+from Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_BLUE
 
 
-class Menu(Game):
-    def __init__(self):
-        self.window = None
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./Assets/Menu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./Assets/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(70, "Alien", COLOR_ORANGE, ((WIN_WIDTH / 2), 50))
+            self.menu_text(70, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 100))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_BLUE, ((WIN_WIDTH / 2), 200 + 20 * i))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
