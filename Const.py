b5e7fab70a755969dec7b0ec642ddcb4411ddd59
LeoAmaral <leoamaral1291@gmail.com>
2025-03-19 19:12:02 -0300

    Creating Players and Enemies Moving players and enemies

--- Const.py
+++ Const.py
@@ -1,15 +1,22 @@
 # C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_BLUE = (0, 0, 255)
 COLOR_YELLOW = (255, 255, 0)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1L0': 0,
     'Level1L1': 1,
     'Level1L2': 1,
     'Level1L3': 2,
     'Level1L4': 2.3,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 
 }
 
@@ -19,6 +26,21 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+# S
+SPAWN_TIME = 4000
 
 # W
 WIN_WIDTH = 576
--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Const import WIN_WIDTH
+import random
+
+from Const import WIN_WIDTH, WIN_HEIGHT
 from code.background import Background
+from code.enemy import Enemy
+from code.player import Player
 
 
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0, 0)):
+    def get_entity(entity_name: str, ):
         match entity_name:
             case 'Level1L':
                 list_l = []
@@ -15,3 +19,11 @@ class EntityFactory:
                     list_l.append(Background(f'Level1L{i}', (0, 0)))
                     list_l.append(Background(f'Level1L{i}', (WIN_WIDTH, 0)))
                 return list_l
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
